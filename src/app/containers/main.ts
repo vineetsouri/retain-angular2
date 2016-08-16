0b4bc969ad62b466080abd13efa7a3833cc364c4
vineetsouri <vineetsouri72@gmail.com>
2016-08-16 14:40:22 +0530

    added nav bar to retain app

--- src/app/containers/main.ts
+++ src/app/containers/main.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AppBar } from '../ui';
 
 @Component({
   selector: 'main-container',
+  directives: [AppBar],
   template: `
   <div>
+    <app-bar></app-bar>
     <main class="main">
       content will go here...
     </main>
