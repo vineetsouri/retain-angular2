08fad209edb03c983a1c2664214b07aae2a1e17c
vineetsouri <vineetsouri72@gmail.com>
2016-08-17 11:15:54 +0530

    created an api service and introduced in the app not completely working. Just set up

--- src/app/ui/note-creator.ts
+++ src/app/ui/note-creator.ts
@@ -22,15 +22,17 @@ import { Component,Output,EventEmitter } from '@angular/core';
           name="newNoteTitle"
           placeholder="Title"
           class="col-xs-10 title"
+          *ngIf="fullForm"
         >
         <input
           type="text"
+          (focus)="toggle(true)"
           [(ngModel)]="newNote.value"
           name="newnoteValue"
           placeholder="Take a note..."
           class="col-xs-10"
         >
-        <div class="actions col-xs-12 row between-xs">
+        <div class="actions col-xs-12 row between-xs" *ngIf="fullForm">
           <button
             type="submit"
             class="btn-light"
@@ -49,6 +51,7 @@ export class NoteCreator {
     title: '',
     value: ''
   };
+  fullForm: boolean = false;
   onCreateNote(){
     const { title,value } = this.newNote;
     if(title && value) {
@@ -62,5 +65,8 @@ export class NoteCreator {
       title: '',
       value: ''
     }
+  }
+  toggle(value: boolean){
+    this.fullForm = value;
   }
 }
